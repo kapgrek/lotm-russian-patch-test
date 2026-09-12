e6a8ef5ecfa47ac8411a5116f9ca4a535efcb9b3
kapgrek <kapgrek@users.noreply.github.com>
2026-09-12 22:54:22 +0300

    chore(release): prepare release v2.6.2-RU

--- installer/AssemblyInfo.cs
+++ installer/AssemblyInfo.cs
@@ -13,5 +13,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("e5868f76-8806-4b2a-87eb-9293a95bfcf2")]
 
-[assembly: AssemblyVersion("2.6.0.0")]
-[assembly: AssemblyFileVersion("2.6.0.0")]
+[assembly: AssemblyVersion("2.6.2.0")]
+[assembly: AssemblyFileVersion("2.6.2.0")]
